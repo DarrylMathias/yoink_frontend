e7d922e7e430979f80a09f527c64522086411351
Darryl Mathias <mathiasndarryl7@gmail.com>
2026-07-02 13:10:35 +0530

    dynamic api url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL;
+
+if (!API_URL) {
+  throw new Error("Missing API URL");
+}
+
 export const apiClient = axios.create({
-  baseURL: 'https://yoink-api.darrylmathias.tech/api',
+  baseURL: API_URL,
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
